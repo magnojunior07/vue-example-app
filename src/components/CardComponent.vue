86599749fa871680dc0e6aa994df36a81846bbb6
magnojunior07 <magnojunior319@gmail.com>
2023-08-18 15:54:34 -0400

    feat: start card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,14 @@
+<script setup-lang="ts">
+  defineProps<{
+    quote: string
+  }>()
+</script>
+
+<template>
+  <div class="bg-gray-200 w-2/5 p-8 rounded-lg mx-auto">
+    <p>
+      {{ props.quote }}
+    </p>
+  </div>  
+</template>
+
