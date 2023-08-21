a68d75168c481390046eea0f4cd6d7184dbf8818
magnojunior07 <magnojunior319@gmail.com>
2023-08-21 13:18:12 -0400

    feat: improve responsivity from card component

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -5,12 +5,12 @@
 </script>
 
 <template>
-  <div class="bg-gray-200 w-2/5 p-8 rounded-lg mx-auto">
+  <div class="bg-gray-200 w-2/5 p-8 rounded-lg mx-auto text-sm sm:text-base">
     <p>
       {{ quote }}
     </p>
-    <p class="text-end pt-2 px-4 text-sm">
-      - Kanye West
+    <p class="text-end pt-2 px-4 text-xs sm:text-sm">
+      - West, Kayne
     </p>
   </div>  
 </template>
