79247ab159599ef9c90764049c622d701a91b4c0
magnojunior07 <magnojunior319@gmail.com>
2023-08-21 13:18:39 -0400

    feat: add getQuote and translateQuote functions

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import axios from 'axios';
 import HeaderComponent from './components/HeaderComponent.vue';
 import CardComponent from './components/CardComponent.vue';
 import ButtonComponent from './components/ButtonComponent.vue';
@@ -8,7 +9,10 @@ import ButtonComponent from './components/ButtonComponent.vue';
   <HeaderComponent />
   <main>
     <CardComponent v-if="quote != ''" v-bind:quote="quote" />
-    <ButtonComponent text="Gerar Quote"/>
+    <div class="flex mx-auto w-1/2 justify-evenly">
+      <ButtonComponent @click="getQuote" class="mt-4" text="Gerar Quote"/>
+      <ButtonComponent @click="translateQuote" class="mt-4" text="Traduzir"/>
+    </div>
   </main>
 </template>
 
@@ -16,8 +20,22 @@ import ButtonComponent from './components/ButtonComponent.vue';
   export default {
   data() {
     return {
-      quote: ""
+      quote: "",
+      image: ""
     }
-  }
+  },
+  methods: {
+    async getQuote() {
+      const response = await axios.get('https://api.kanye.rest/');
+      this.quote = response.data.quote;
+    },
+    async translateQuote() {
+        const response = await axios.get(
+          `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=pt&dt=t&q=${encodeURIComponent(this.quote)}`
+        );
+        const translatedQuote = await response.data;
+        this.quote = translatedQuote[0][0][0];
+    }
+  },
 }
 </script>
